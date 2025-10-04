0181b9a6bb0eba6e372ea4c72911ed02dc2ae2ba
Ayushraj1758 <ayushraj1758@gmail.com>
2025-10-04 17:36:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (66.76%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)return 0;
+        if(n==1 || n==2)return 1;
+        
+        // return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        
+        int a=0,b=1,c=1,next=0;;
+        for(int i=3;i<=n;i++){
+            next=a+b+c;
+            a=b;b=c;c=next;
+        }
+        return next;
+    }
+};
