a5d417ca0e67e07f6659baa13acc2820951db929
Ayushraj1758 <ayushraj1758@gmail.com>
2025-12-12 13:42:01 +0530

    Time: 23 ms (50.27%) | Memory: 17.4 MB (82.90%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,3 +1,37 @@
+// class Solution {
+// public:
+//     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector<vector<int>> ans;
+//         sort(nums.begin(), nums.end());
+
+//         for (int i = 0; i < n - 3; ++i) {
+//             if (i > 0 && nums[i] == nums[i-1]) continue;        // skip i-duplicates
+//             for (int j = i + 1; j < n - 2; ++j) {
+//                 if (j > i + 1 && nums[j] == nums[j-1]) continue; // skip j-duplicates
+
+//                 int k = j + 1, l = n - 1;
+//                 while (k < l) {
+//                     long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+//                     if (sum == target) {
+//                         ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+//                         ++k; --l;
+//                         // skip duplicates for k and l after moving them
+//                         while (k < l && nums[k] == nums[k-1]) ++k;
+//                         while (k < l && nums[l] == nums[l+1]) --l;
+//                     } else if (sum < target) {
+//                         ++k;
+//                     } else {
+//                         --l;
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// take the sum into long long
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
