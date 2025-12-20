5527084c5b578e44c54a364d2630c72ad6a48084
Ayushraj1758 <ayushraj1758@gmail.com>
2025-12-21 01:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (79.58%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n =nums.size();
-        int low=0;int high=n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)return mid;
+    int recsearch(vector<int>& nums,int low,int high,int target){
+        if(low>high){
+            return -1;
 
-            if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
         }
-        return -1;
+    
+        int mid=(low+high)/2;
+        if(nums[mid]==target)return mid;
+        
+        else if(nums[mid]>target)
+        return recsearch(nums,low,mid-1,target);
+        else 
+        return recsearch(nums,mid+1,high,target);
+        
+    }
+    int search(vector<int>& nums, int target) {
+        //it is used only for the sorted array and the time complexity for this type of search is log N
+        int n =nums.size();
+        return recsearch(nums,0,n-1,target);
+
+
+
+        //iterative approach
+        // int low=0;int high=n-1;
+        // while(low<=high){
+        //     int mid=(low+high)/2;
+        //     if(nums[mid]==target)return mid;
+
+        //     if(nums[mid]<target){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return -1;
 
     }
 };
