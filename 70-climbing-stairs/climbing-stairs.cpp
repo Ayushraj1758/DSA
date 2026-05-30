6a9d6f0174d17837db371933e326ef21fbcedb04
Ayushraj1758 <ayushraj1758@gmail.com>
2026-05-30 22:45:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,4 +11,14 @@ public:
         return b;
         
     }
+    // int f(int ind){
+    //     if(ind==0 || ind==1 || ind==2)return ind;
+    //     // int left=f(ind-1);
+    //     // int right = f(ind-2);
+    //     return f(ind-1)+f(ind-2);
+    // }
+    // int climbStairs(int n){
+    //     return f(n);
+
+    // }
 };
