27aa6a3b86b99892727acb2a2f46ea97e49f1008
Ayushraj1758 <ayushraj1758@gmail.com>
2026-07-27 08:48:22 +0530

    Time: 43 ms (25.20%) | Memory: 27.7 MB (6.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+    int f(string &t1,string &t2,int ind1,int ind2,vector<vector<int>> &dp){
+        if(ind1<0  || ind2 < 0)return 0;
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+
+        if(t1[ind1]==t2[ind2])return dp[ind1][ind2]=1+f(t1,t2,ind1-1,ind2-1,dp);
+        return dp[ind1][ind2]=0+max(f(t1,t2,ind1-1,ind2,dp),f(t1,t2,ind1,ind2-1,dp));
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<int> dp(text1.length(), 0);
-        int longest = 0;
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return f(text1,text2,n1-1,n2-1,dp);
 
-        for (char c : text2) {
-            int curLength = 0;
-            for (int i = 0; i < dp.size(); i++) {
-                if (curLength < dp[i]) {
-                    curLength = dp[i];
-                } else if (c == text1[i]) {
-                    dp[i] = curLength + 1;
-                    longest = max(longest, curLength + 1);
-                }
-            }
-        }
-
-        return longest;        
+       
     }
 };
