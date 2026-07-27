0749fcd7ea69c6f705091dbeaf279d48054eebf6
Ayushraj1758 <ayushraj1758@gmail.com>
2026-07-27 09:43:17 +0530

    Time: 23 ms (81.32%) | Memory: 27.5 MB (31.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,38 @@
-class Solution {
-public:
-    int f(string &t1,string &t2,int ind1,int ind2,vector<vector<int>> &dp){
-        if(ind1<0  || ind2 < 0)return 0;
-        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+// class Solution {
+// public:
+//     int f(string &t1,string &  t2,int ind1,int ind2,vector<vector<int>> &dp){
+//         if(ind1<0  || ind2 < 0)return 0;
+//         if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
 
-        if(t1[ind1]==t2[ind2])return dp[ind1][ind2]=1+f(t1,t2,ind1-1,ind2-1,dp);
-        return dp[ind1][ind2]=0+max(f(t1,t2,ind1-1,ind2,dp),f(t1,t2,ind1,ind2-1,dp));
-    }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n1=text1.size();
-        int n2=text2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return f(text1,text2,n1-1,n2-1,dp);
+//         if(t1[ind1]==t2[ind2])return dp[ind1][ind2]=1+f(t1,t2,ind1-1,ind2-1,dp);
+//         return dp[ind1][ind2]=0+max(f(t1,t2,ind1-1,ind2,dp),f(t1,t2,ind1,ind2-1,dp));
+//     }
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n1=text1.size();
+//         int n2=text2.size();
+//         vector<vector<int>>dp(n1,vector<int>(n2,-1));
+//         return f(text1,text2,n1-1,n2-1,dp);
 
        
-    }
+//     }
+// };
+
+//tabulation
+class Solution {
+    public:
+        int longestCommonSubsequence(string text1, string text2){
+            int n1=text1.size();
+            int n2=text2.size();
+            if(n1==0 || n2==0 )return 0;
+            vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+            for(int i=1;i<=n1;i++){
+                for(int j=1;j<=n2;j++){
+                    if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                    else
+                    dp[i][j]=0+max(dp[i-1][j],dp[i][j-1]);
+   
+                }
+            }
+            return dp[n1][n2];
+        }
 };
