f35e1cc777e5b5453da677c333511168f7bfa9ce
Ayushraj1758 <ayushraj1758@gmail.com>
2026-08-20 17:38:45 +0530

    Time: 65 ms (74.80%) | Memory: 193.6 MB (35.43%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,49 +1,38 @@
 class Solution {
-    const int mod = 1000000007;
-
+    const int mod=1000000007;
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        long long ans = 0;
-
-        vector<int> nse(n), pse(n);
+        int ans=0;
+        int n=arr.size();
+        vector<int> nse(n),pse(n);
         stack<int> st;
         stack<int> st2;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i])st.pop();
+            if(st.empty())nse[i]=n;
+            else{
+                nse[i]=st.top();
 
-        // Next Smaller Element
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i])
-                st.pop();
-
-            if (st.empty())
-                nse[i] = n;
-            else
-                nse[i] = st.top();
-
+            }
             st.push(i);
         }
 
-        // Previous Smaller Element
-        for (int i = 0; i < n; i++) {
-            while (!st2.empty() && arr[st2.top()] > arr[i])
-                st2.pop();
-
-            if (st2.empty())
-                pse[i] = -1;
-            else
-                pse[i] = st2.top();
-
+        for(int i=0;i<n;i++){
+            while(!st2.empty() && arr[st2.top()]>arr[i])st2.pop();
+            if(st2.empty())pse[i]=-1;
+            else{
+                pse[i]=st2.top();
+            }
             st2.push(i);
+
         }
-
-        // Contribution of each element
-        for (int i = 0; i < n; i++) {
-            long long left = i - pse[i];
-            long long right = nse[i] - i;
-
-            ans = (ans + left * right * arr[i]) % mod;
+        for(int i=0;i<n;i++){
+            long  left=i-pse[i];
+            long right=nse[i]-i;
+            ans=(ans+left*right* arr[i])%mod;
         }
-
-        return ans;
+        
+        return ans ;
+        
     }
 };
