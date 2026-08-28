7682c5f0eceb0d9b396d53b094a775f409bf5d4b
Ayushraj1758 <ayushraj1758@gmail.com>
2026-08-28 20:46:49 +0530

    Time: 8 ms (10.34%) | Memory: 73.6 MB (7.06%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -14,12 +14,17 @@ public:
         }
 
         for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1])right[i]=right[i+1]+1;
+            if(ratings[i]>ratings[i+1]){
+            left[i]=max(left[i],left[i+1]+1);   
+             
+            // right[i]=right[i+1]+1;
+            }
        
         }
        
         for(int i=0;i<n;i++){
-            sum+=max(left[i],right[i]);
+            // sum+=max(left[i],right[i]);
+            sum+=left[i];
         }
         return sum;
         
