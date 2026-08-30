4827c2304017465d6695a6aab8ca1e13b3bf7f21
Ayushraj1758 <ayushraj1758@gmail.com>
2026-08-30 12:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (58.39%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,17 +3,34 @@ public:
 
 string longestCommonPrefix(vector<string>& strs) {
     if (strs.empty()) return "";
+    int n=strs.size();
     
-    string prefix = strs[0];
+    // string prefix = strs[0];
     
-    for (int i = 1; i < strs.size(); ++i) {
-        while (strs[i].find(prefix) != 0) {
-            prefix = prefix.substr(0, prefix.size() - 1);
-            if (prefix.empty()) return "";
+    // for (int i = 1; i < strs.size(); ++i) {
+    //     while (strs[i].find(prefix) != 0) {
+    //         prefix = prefix.substr(0, prefix.size() - 1);
+    //         if (prefix.empty()) return "";
+    //     }
+    // }
+    // return prefix;
+    string st=strs[0],ans="";
+    for(int i=0;i<strs[0].size();i++){
+        // char x=st[i];
+        bool temp=true;
+        for(int j=1;j<n;j++){
+            if(st[i]!=strs[j][i]){
+                temp=false;break;
+            }
         }
+        if(!temp)break;
+        else{
+            ans+=st[i];
+        }
+        
     }
-    
-    return prefix;
+    return ans;
+
 }
         
     
