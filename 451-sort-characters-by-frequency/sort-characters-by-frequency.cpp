02c7552b99fdc2788acc60bd144a85b6269755d2
Ayushraj1758 <ayushraj1758@gmail.com>
2026-08-30 10:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (25.48%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        int n=s.size();
+        map<char,int>mpp;
+        for(auto c:s){
+            mpp[c]++;
+
+        }
+        vector<pair<char,int>> v;
+        for(auto it : mpp){
+            v.push_back({it.first,it.second});
+        }
+        sort(v.begin(),v.end(),[](pair<char,int>& a,pair<char,int>&b){
+            return a.second>b.second;
+        });
+        string ans="";
+        for(auto it:v){
+            ans+=string(it.second,it.first);
+        }
+        return ans;
+
+    }
+};
