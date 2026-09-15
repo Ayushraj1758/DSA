21b794aad75549a3f10de13e381930088d9d913f
Ayushraj1758 <ayushraj1758@gmail.com>
2026-09-15 09:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (25.90%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,34 +13,54 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(head==NULL)return head;
         ListNode* temp=head;
-        ListNode* prev=NULL;
-        while(temp){
-            ListNode* x=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=x;
 
-        }
-        temp=prev;
-        if(n==1)prev=prev->next;
+
+        //By reversing technique
+
+        // ListNode* prev=NULL;
+        // while(temp){
+        //     ListNode* x=temp->next;
+        //     temp->next=prev;
+        //     prev=temp;
+        //     temp=x;
+
+        // }
+        // temp=prev;
+        // if(n==1)prev=prev->next;
+        // int cnt=0;
+        // while(temp){
+        //     cnt++;
+        //     if(cnt==n-1){
+        //         temp->next=temp->next->next;
+        //     }
+        //     temp=temp->next;
+        // }
+        // temp=prev;
+        // ListNode* prev2=NULL;
+        // while(temp){
+        //     ListNode* x=temp->next;
+        //     temp->next=prev2;
+        //     prev2=temp;
+        //     temp=x;
+
+        // }
+        // return prev2;
+
         int cnt=0;
         while(temp){
             cnt++;
-            if(cnt==n-1){
-                temp->next=temp->next->next;
-            }
             temp=temp->next;
         }
-        temp=prev;
-        ListNode* prev2=NULL;
-        while(temp){
-            ListNode* x=temp->next;
-            temp->next=prev2;
-            prev2=temp;
-            temp=x;
-
+        if(n==cnt)return head->next; 
+        temp=head;
+        int res=cnt-n;
+        while(res){
+            res--;
+            if(res)
+            temp=temp->next;
         }
-        return prev2;
-        
+        temp->next=temp->next->next;
+
+        return head;
     }
 };
